fb4b0cc094ee4c5464efb1a38938b884493dea39
kevengarcyya <kevengarcyya@gmail.com>
2026-03-17 21:54:32 -0300

    comiti inicial

---
+++ src/main/java/com/amil/e_comerce_backend/Application.java
@@ -0,0 +1,13 @@
+package com.amil.e_comerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amil/e_comerce_backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amil.e_comerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
