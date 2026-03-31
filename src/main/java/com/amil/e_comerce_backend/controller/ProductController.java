da3f4082950d170bf3161ac6108a0b673846f259
kevengarcyya <kevengarcyya@gmail.com>
2026-03-31 08:34:40 -0300

    fix: implement metodos em ProductController.

--- src/main/java/com/amil/e_comerce_backend/controller/ProductController.java
+++ src/main/java/com/amil/e_comerce_backend/controller/ProductController.java
@@ -1,13 +1,13 @@
 package com.amil.e_comerce_backend.controller;
 
 
+import com.amil.e_comerce_backend.dto.ClientDTO;
 import com.amil.e_comerce_backend.dto.ProductDTO;
 import com.amil.e_comerce_backend.servics.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/product")
 @RestController
@@ -20,4 +20,30 @@ public class ProductController {
     public void insert(@RequestBody ProductDTO productDTO){
         productService.insert(productDTO);
     }
+
+    @GetMapping
+    public List<ProductDTO> findAllClient() {
+        return productService.findAll();
+    }
+
+    @PutMapping
+    public ProductDTO editClient(@RequestBody ProductDTO productDTO) {
+        return productService.edit(productDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteClient(@PathVariable Long id) {
+        productService.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public ProductDTO findByIdClient(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @GetMapping("/findByName")
+    public List<ProductDTO> findByNameClient(@RequestParam String firstName){
+        return productService.findByName(firstName);
+    }
+
 }
