a7245304394598f74f0e1b9a2b83473cfbd43598
kevengarcyya <kevengarcyya@gmail.com>
2026-03-31 08:29:14 -0300

    fix: implement metodos em ProductService.

--- src/main/java/com/amil/e_comerce_backend/repository/ProductRepository.java
+++ src/main/java/com/amil/e_comerce_backend/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.amil.e_comerce_backend.repository;
 import com.amil.e_comerce_backend.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    public List<ProductEntity> findByNameContaining(String name);
 }
--- src/main/java/com/amil/e_comerce_backend/servics/ProductService.java
+++ src/main/java/com/amil/e_comerce_backend/servics/ProductService.java
@@ -1,10 +1,14 @@
 package com.amil.e_comerce_backend.servics;
 
+import com.amil.e_comerce_backend.dto.ClientDTO;
 import com.amil.e_comerce_backend.dto.ProductDTO;
+import com.amil.e_comerce_backend.entity.ClientEntity;
 import com.amil.e_comerce_backend.entity.ProductEntity;
 import com.amil.e_comerce_backend.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -15,6 +19,30 @@ public class ProductService {
     public void insert(ProductDTO productDTO){
         ProductEntity productEntity = new ProductEntity(productDTO);
         productRepository.save(productEntity);
+    }
+
+    public List<ProductDTO> findAll(){
+        List<ProductEntity> product = productRepository.findAll();
+        return product.stream().map(ProductDTO::new).toList();
+    }
+
+    public ProductDTO edit(ProductDTO productDTO){
+        ProductEntity product = new ProductEntity(productDTO);
+        return new ProductDTO(productRepository.save(product));
+    }
+
+    public void delete(Long id){
+        productRepository.deleteById(id);
+    }
+
+    public ProductDTO findById(Long id){
+        ProductEntity product = productRepository.findById(id).get();
+        return new ProductDTO(product);
+    }
+
+    public List<ProductDTO> findByName(String name){
+        List<ProductEntity> product = productRepository.findByNameContaining(name);
+        return product.stream().map(ProductDTO::new).toList();
     }
 
 }
