fd2e45f1a935d01ee7202e7e450adaa8c0469129
kevengarcyya <kevengarcyya@gmail.com>
2026-04-09 23:53:03 -0300

    docs: implements documentacao na VendasController.

--- src/main/java/com/amil/e_comerce_backend/controller/VendasController.java
+++ src/main/java/com/amil/e_comerce_backend/controller/VendasController.java
@@ -3,11 +3,14 @@ package com.amil.e_comerce_backend.controller;
 import com.amil.e_comerce_backend.dto.VendasDTO;
 import com.amil.e_comerce_backend.repository.VendasRepository;
 import com.amil.e_comerce_backend.servics.VendasService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Vendas", description = "Gerenciamento do registro de vendas")
 @RequestMapping("/vendas")
 @RestController
 public class VendasController {
@@ -16,13 +19,14 @@ public class VendasController {
     private VendasService vendasService;
 
     @PostMapping
+    @Operation(summary = "Registrar", description = "Registre uama nova venda.")
     public void insert(@RequestBody VendasDTO vendasDTO){
         vendasService.insert(vendasDTO);
     }
 
     @GetMapping("/buscar_registros/{id}")
+    @Operation(summary = "Registro por cliente", description = "Busca registros de vendas ligadas a um cliente.")
     public List<VendasDTO> findVendasClient(@PathVariable Long id){
-
         return vendasService.findByregistros(id);
     }
 
